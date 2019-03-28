abc7781c3bbe368603a5234cf41672aff814c7d1
Iksan Maricar <iksan.maricar@futuready.com>
2019-03-28 18:13:03 +0700

    remote npm install

--- deploy/run.sh
+++ deploy/run.sh
@@ -2,5 +2,4 @@ source /home/ec2-user/.bash_profile
 # cd /home/ec2-user/fbl/be && npm install
 # pm2 start /home/ec2-user/fbl/be/index.js
 
-cd /var/www/html/fbl-fe && npm install
-npm run serve > /dev/null &
+cd /var/www/html/fbl-fe
