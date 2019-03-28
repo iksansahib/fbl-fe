b9b5a81a9ad6fd1faba657e5c3e9288be7a77444
Iksan Maricar <iksan.maricar@futuready.com>
2019-03-28 18:28:45 +0700

    add remove folder

--- deploy/before_install.sh
+++ deploy/before_install.sh
@@ -6,5 +6,6 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 nvm install 10.15
 
 sudo service httpd start
+sudo rm -rf /var/www/html/fbl-fe
 # npm install -g pm2
 # pm2 update
